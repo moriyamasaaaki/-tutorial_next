4b17481adfe6fed18effcd68a85fe4219f5c0a81
moriyamasaaaki <moriya-7071@outlook.com>
2020-06-01 01:44:39 +0900

    feat(components,pages,actions): カテゴリーの取得 #5

--- components/sideMenu.js
+++ components/sideMenu.js
@@ -1,19 +1,16 @@
 import { useState } from "react";
 
 const SideMenu = (props) => {
+  const { categories } = props;
   return (
     <div>
       <h1 className="my-4">{props.appName}</h1>
       <div className="list-group">
-        <a href="#" className="list-group-item">
-          カテゴリー 1
-        </a>
-        <a href="#" className="list-group-item">
-          カテゴリー 2
-        </a>
-        <a href="#" className="list-group-item">
-          カテゴリー 3
-        </a>
+        {categories.map((c) => (
+          <a key={c.id} href="#" className="list-group-item">
+            {c.name}
+          </a>
+        ))}
       </div>
     </div>
   );
