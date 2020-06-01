3569a239cd032539bececf830ffc7a1a08648b7f
moriyamasaaaki <moriya-7071@outlook.com>
2020-06-01 17:38:18 +0900

    feat(actions,components): フォームの作成 #8

---
+++ components/movieCreateForm.js
@@ -0,0 +1,149 @@
+import { useState } from "react";
+
+const MovieCreatehtmlForm = (props) => {
+  const [htmlForm, sethtmlForm] = useState({
+    name: "",
+    description: "",
+    rating: "",
+    image: "",
+    cover: "",
+    longDesc: "",
+  });
+
+  const handleChange = (event) => {
+    const target = event.target;
+    const name = target.name;
+
+    sethtmlForm({
+      ...htmlForm,
+      [name]: target.value,
+    });
+  };
+
+  const handleGenreChange = (event) => {
+    const { options } = event.target;
+    const optionsLength = options.length;
+    let value = [];
+
+    for (let i = 0; i < optionsLength; i++) {
+      if (options[i].selected) {
+        value.push(options[i].value);
+      }
+    }
+
+    sethtmlForm({
+      ...htmlForm,
+      genre: value.toString(),
+    });
+  };
+
+  const submithtmlForm = () => {
+    props.handleFormSubmit({ ...htmlForm });
+  };
+
+  return (
+    <form>
+      <div className="htmlForm-group">
+        <label htmlFor="name">名前</label>
+        <input
+          onChange={handleChange}
+          value={htmlForm.name}
+          name="name"
+          type="text"
+          className="htmlForm-control"
+          id="name"
+          aria-describedby="emailHelp"
+          placeholder="Lord of the Rings"
+        />
+      </div>
+      <div className="htmlForm-group">
+        <label htmlFor="description">概要</label>
+        <input
+          onChange={handleChange}
+          value={htmlForm.description}
+          name="description"
+          type="text"
+          className="htmlForm-control"
+          id="description"
+          placeholder="Somewhere in Middle-earth..."
+        />
+      </div>
+      <div className="htmlForm-group">
+        <label htmlFor="description">評価</label>
+        <input
+          onChange={handleChange}
+          value={htmlForm.rating}
+          name="rating"
+          type="number"
+          max="5"
+          min="0"
+          className="htmlForm-control"
+          id="rating"
+          placeholder="3"
+        />
+        <small id="emailHelp" className="htmlForm-text text-muted">
+          Max: 5, Min: 0{" "}
+        </small>
+      </div>
+      <div className="htmlForm-group">
+        <label htmlFor="image">画像</label>
+        <input
+          onChange={handleChange}
+          value={htmlForm.image}
+          name="image"
+          type="text"
+          className="htmlForm-control"
+          id="image"
+          placeholder="http://....."
+        />
+      </div>
+      <div className="htmlForm-group">
+        <label htmlFor="cover">カバー画像</label>
+        <input
+          onChange={handleChange}
+          value={htmlForm.cover}
+          name="cover"
+          type="text"
+          className="htmlForm-control"
+          id="cover"
+          placeholder="http://......"
+        />
+      </div>
+      <div className="htmlForm-group">
+        <label htmlFor="longDesc">内容</label>
+        <textarea
+          onChange={handleChange}
+          value={htmlForm.longDesc}
+          name="longDesc"
+          className="htmlForm-control"
+          id="longDesc"
+          rows="3"
+        ></textarea>
+      </div>
+      <div className="htmlForm-group">
+        <label htmlFor="genre">ジャンル</label>
+        <select
+          onChange={handleGenreChange}
+          multiple
+          className="htmlForm-control"
+          id="genre"
+        >
+          <option>drama</option>
+          <option>music</option>
+          <option>adventure</option>
+          <option>historical</option>
+          <option>action</option>
+        </select>
+      </div>
+      <button
+        onClick={submithtmlForm}
+        type="button"
+        className="btn btn-primary"
+      >
+        作成
+      </button>
+    </form>
+  );
+};
+
+export default MovieCreatehtmlForm;
